ca929c4c72b659043abb0b854b2f7e13ac0d3e1e
pberube12 <pberube12@gmail.com>
2013-05-28 12:56:26 -0400

    À voir
    Besoin d'un peu d'aide mes amis :S

--- avidQtJpegFonction.py
+++ avidQtJpegFonction.py
@@ -20,45 +20,57 @@ listAvidPath = os.listdir(avidPath)
 listJpegPath = os.listdir(jpegPath)
 
 # fonction pour changer le nom du quicktime en lui rajoutant _AVID
-# Path des qts voulu en Avid
-originalPath = sys.argv[1]
-for files in os.listdir(originalPath):
-    filename = files.split(".")[0]
-    ext = files.split(".")[-1]
-    qtAvid = (filename + "_AVID")
-    qtJpeg = (filename + ".zip")
-    if ext.lower() != "mov":
-        continue
-    filenameAvid = qtAvid + "." + ext
-    print files
+def renameAvid(originalPath):
+    listOriginalPath = os.listdir(originalPath)
+    for files in listOriginalPath:
+        filename = files.split(".")[0]
+        ext = files.split(".")[-1]
+        filenameAvid = (filename + "_AVID") # nom temporaire pour rajouter le _AVID au nom
+        if ext.lower() != "mov":
+            continue
+        qtAvid = filenameAvid + "." + ext
+        return qtAvid
 
-# la partie qui va trouver le Qt AVid et le copy au path voulu
-    for newFile in listAvidPath:
-        fullPath = os.path.join(avidPath, newFile)
-        destPathMxf = os.path.join(sys.argv[2], "MXF")
-        if not os.path.exists(destPathMxf):
-            os.makedirs(destPathMxf)
-        if os.path.isdir(fullPath):
+# fonction pour changer le nom des quicktime en remplacant le mov en zip
+def renameZip(originalPath):
+    listOriginalPath = os.listdir(originalPath)
+    for files in listOriginalPath:
+        filename = files.split(".")[0]
+        qtJpeg = (filename + ".zip")
+        ext = files.split(".")[-1]
+        if ext.lower() != "mov":
             continue
-        if filenameAvid.lower() == newFile.lower():
-            print(filenameAvid)
-            print("Searching Qt Avid")
-            shutil.copy2(fullPath, destPathMxf)
-            print("Done")
-        else:
-            continue
-        
-    for newZip in listJpegPath:
-        fullPathZip = os.path.join(jpegPath, newZip)
-        destPathZip = os.path.join(sys.argv[2], "JPEG")
-        if not os.path.exists(destPathZip):
-            os.makedirs(destPathZip)
-        if os.path.isdir(fullPathZip):
-            continue
-        if qtJpeg.lower() == newZip.lower():
-            print(qtJpeg)
-            print("Searching Zip")
-            shutil.copy2(fullPathZip, destPathZip)
-            print("Done")
-        else:
-            continue
+        return qtJpeg
+    
+
+# la partie qui va trouver le Qt Avid et le copy au path voulu
+for newFile in listAvidPath:
+    fullPath = os.path.join(avidPath, newFile)
+    destPathMxf = os.path.join(sys.argv[2], "MXF")
+    if not os.path.exists(destPathMxf):
+        os.makedirs(destPathMxf)
+    if os.path.isdir(fullPath):
+        continue
+    if renameAvid(sys.argv[1]) == newFile.lower():
+        print("")
+        print("Searching Qt Avid")
+        shutil.copy2(fullPath, destPathMxf)
+        print("Done")
+    else:
+        continue
+
+# la partie qui va trouver les zip et le copy au path voulu
+for newZip in listJpegPath:
+    fullPathZip = os.path.join(jpegPath, newZip)
+    destPathZip = os.path.join(sys.argv[2], "JPEG")
+    if not os.path.exists(destPathZip):
+        os.makedirs(destPathZip)
+    if os.path.isdir(fullPathZip):
+        continue
+    if renameZip(sys.argv[1]) == newZip.lower():
+        print("")
+        print("Searching Zip")
+        shutil.copy2(fullPathZip, destPathZip)
+        print("Done")
+    else:
+        continue
