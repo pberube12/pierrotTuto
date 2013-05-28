c841f7ece8661f4c31756b36c79dc6e2664ee9dc
pberube12 <pberube12@gmail.com>
2013-05-28 10:54:58 -0400

    Modif new folder
    Copie les fichier avid et zip dans des nouveaux folders

--- avidQtJpeg.py
+++ avidQtJpeg.py
@@ -19,42 +19,43 @@ jpegPath = ("//Volumes//IO01//HyMovieCreator//Quicktime//RAR")
 # fonction pour changer le nom du quicktime en lui rajoutant _AVID
 # Path des qts voulu en Avid
 originalPath = sys.argv[1]
-def renameFile():
-    for files in os.listdir(originalPath):
-        filename = files.split(".")[0]
-        ext = files.split(".")[-1]
-        qtAvid = (filename + "_AVID")
-        qtJpeg = (filename + ".zip")
-        if ext.lower() != "mov":
-            continue
-        filenameAvid = qtAvid + "." + ext
-        print files
-        print("allo")
+for files in os.listdir(originalPath):
+    filename = files.split(".")[0]
+    ext = files.split(".")[-1]
+    qtAvid = (filename + "_AVID")
+    qtJpeg = (filename + ".zip")
+    if ext.lower() != "mov":
+        continue
+    filenameAvid = qtAvid + "." + ext
+    print files
 
 # la partie qui va trouver le Qt AVid et le copy au path voulu
     for newFile in os.listdir(avidPath):
         fullPath = os.path.join(avidPath, newFile)
-        destPathZip = os.path.join(sys.argv[2], "AVID")
-        if not os.path.exists(destPathZip):
-            os.makedirs(destPathZip)
+        destPathMxf = os.path.join(sys.argv[2], "MXF")
+        if not os.path.exists(destPathMxf):
+            os.makedirs(destPathMxf)
         if os.path.isdir(fullPath):
             continue
         if filenameAvid.lower() == newFile.lower():
             print(filenameAvid)
             print("Searching Qt Avid")
-            shutil.copy2(fullPath, sys.argv[2])
+            shutil.copy2(fullPath, destPathMxf)
             print("Done")
         else:
             continue
         
     for newZip in os.listdir(jpegPath):
         fullPathZip = os.path.join(jpegPath, newZip)
+        destPathZip = os.path.join(sys.argv[2], "JPEG")
+        if not os.path.exists(destPathZip):
+            os.makedirs(destPathZip)
         if os.path.isdir(fullPathZip):
             continue
         if qtJpeg.lower() == newZip.lower():
             print(qtJpeg)
             print("Searching Zip")
-            shutil.copy2(fullPathZip, sys.argv[2])
+            shutil.copy2(fullPathZip, destPathZip)
             print("Done")
         else:
             continue
