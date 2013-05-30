d7acf4d2b7cad932934cfb1dcc09de9ceec86ef2
pberube12 <pberube12@gmail.com>
2013-05-30 11:34:32 -0400

    petit menage
    deleted des trucs pu important

--- ex75.py
+++
@@ -1,9 +0,0 @@
-def question(annonce, essais =4, please ='oui ou non'):
-    while essais >0:
-        reponse = input(annonce)
-        if reponse in ('o', 'oui', 'O', 'Oui', 'OUI'):
-            return 1
-        if reponse in ('n', 'non', 'N', 'Non', 'NON'):
-            return 0
-        print(please)
-        essais = essais-1
