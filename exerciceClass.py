90c2b0f79c68474e72ee90053975c61d776052b6
pberube12 <pberube12@gmail.com>
2013-05-31 09:49:33 -0400

    Exercice demandé par Titou
    donc script qui renomme les qt dans un folder les met dans un
    dictionnaire et va recherche les qt dans By_Name et les met en value
    dans le dict

--- exerciceClass.py
+++ exerciceClass.py
@@ -10,15 +10,39 @@
 
 import sys
 import os
-
-path = ("//Users//assistant//Desktop//Not_Delete")
-qtPath = os.listdir(path)
-qtDict = {}
+import pierrotsModules
+"""
+premier step:
+boucle à travers un folder et va print dans un dictionnaire la shot en key et les qt correspondant dans By_Name dans la value
+    qtDict : Sera mon dictionnaire plus tard
+    originalPath : path des quicktime = Key
+    
+    """
 if __name__ == '__main__':
-    for qt in qtPath:
-        fullQtPath = os.path.join(path, qt)
-        if os.path.isdir(fullQtPath):
+    qtDict = {} 
+    originalPath = ("//Users//assistant//Desktop//Not_Delete")
+    path = os.listdir(originalPath)
+# ici nous allons changer le nom des qt
+    for qt in path:
+        listShot = []
+        listScene = []
+        qtPath = os.path.join(originalPath, qt)
+        if os.path.isdir(qtPath):
             continue
-        qtShrut = qt.split("_h")[0]
-        qtDict[qtShrut] = qt
-    print(qtDict)
+        shot = qt.split("_")[0][0:3]
+        scene = qt.split("_")[1]
+        version = qt.split("_")[-1][3:-4]
+        ext = qt.split(".")[-1]
+        newShot = ("SF" + shot + "_" + scene)
+        newScene = ("SF" + shot)
+        listShot.append(newShot) # liste des shot ex. SF003_003
+        listScene.append(newScene) # liste des scene ex. SF003
+# ici c'est la partie qui va chercher dans ByName les bon qt
+        pathBn = ("//Volumes//QUICKTIME//Smurf//Sort_By_Name")
+        for newQt in listScene:
+            pathBn = os.path.join(pathBn, newQt)
+            for newQtDeux in listShot:
+                pathBn = os.path.join(pathBn, newQtDeux)
+                listPathBn = os.listdir(pathBn)
+                qtDict[newShot] = listPathBn
+                print(qtDict)
