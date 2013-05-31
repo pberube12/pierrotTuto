9fe0501b3ec5ffe0f4fc7b01a42bf8d18e665349
pberube12 <pberube12@gmail.com>
2013-05-31 12:10:05 -0400

    Commencement
    Voici le debut du script pas terminé

--- exerciceClass.py
+++ exerciceClass.py
@@ -12,38 +12,28 @@ import sys
 import os
 import pierrotsModules
 """
-premier step:
-boucle à travers un folder et va print dans un dictionnaire la shot en key et les qt correspondant dans By_Name dans la value
+script qui print dans un dictionnaire la shot en key et les qt correspondant dans By_Name dans la value
     qtDict : Sera mon dictionnaire plus tard
-    originalPath : path des quicktime = Key
-    
+    qtRename : Dictionnaire des qt renommé
+    pathBn : path de Sort_By_Name
+    path : path de vision 2D
+    listPathBn : la liste de toutes les shots dans le folder Sort_By_Name
     """
 if __name__ == '__main__':
-"""
-    qtDict = {} 
-    originalPath = ("//Users//assistant//Desktop//Not_Delete")
-    path = os.listdir(originalPath)
-# ici nous allons changer le nom des qt
-    for qt in path:
-        dictByName = {}
-        qtPath = os.path.join(originalPath, qt)
-        if os.path.isdir(qtPath):
+    pathBn = ("//Volumes//QUICKTIME//Smurf//Sort_By_Name")
+    path = ("//Volumes//QUICKTIME//Smurf//Visionnement//2D")
+    listPath = os.listdir(path)
+    qtDict = {}
+    qtRename = pierrotsModules.renameFrums(path)
+    qtRenameShot = qtRename.keys()
+    qtRenameScene = qtRename.values()
+    for newQt in listPath:
+        newQtSplit = newQt.split(".")[-1]
+        if newQtSplit != "mov":
             continue
-        qtSplitter = qt.split("_")
-        erreur = len(qtSplitter)
-        if erreur != 3:
-            continue 
-        shot = qtSplitter[0][0:3]
-        scene = qtSplitter[1]
-        newShot = ("SF" + shot + "_" + scene)
-        newScene = ("SF" + shot)
-        dictByName[newScene] = newShot
-    """
-# ici c'est la partie qui va chercher dans ByName les bon qt
-        pathBn = ("//Volumes//QUICKTIME//Smurf//Sort_By_Name")
-        for newQt in listScene:
-            pathBn = os.path.join(pathBn, newQt)
-            for newQtDeux in listShot:
-                pathBn = os.path.join(pathBn, newQtDeux)
-                listPathBn = os.listdir(pathBn)
-                qtDict[newShot] = listPathBn
+        print newQt
+"""
+        pathBn = os.path.join(pathBn, newQt)
+        listPathBn = os.listdir(pathBn)
+        qtDict[newQt] = listPathBn
+"""
