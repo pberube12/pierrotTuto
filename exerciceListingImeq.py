b47e61614b3e01fd18fca4c04807469bb76564f2
pberube12 <pberube12@gmail.com>
2013-06-04 15:09:30 -0400

    Modif 2.2
    Je dois corriger le fait que je ne vois pas tous les fichiers

--- exerciceListingImeq.py
+++ exerciceListingImeq.py
@@ -12,39 +12,58 @@ import sys
 import os
 import pierrotsModules
 
-def listingFolder(dirSize = None, nbMax = "mov", extension = None):
+def listingFolder(size = False, nbMax = False, extension = False):
     """
     Scrip qui liste un folder et tous les sous folders, retourne une liste de dictionnaire des fichiers avec infos. 
     Ensuite il devra avoir des spécification au recherche, choisir qu'elle extension on veut chercher, la taille des fichier ou non
     le nombre maximal de sous folder à lister
-        fichierDict = Dictionnaire comprennant name, extension, size, path, pour chaque fichier 
         listeFichierDict = la liste de dictionnaire
-        digitListe = la liste des chiffre qui compose l'image séquence
+        imageDict = dictionnaire pour mes images sequences
         dirSize = le poids des fichiers
         pathOriginal = le listing de tous les fichier
         path, dir, files = les path de tout le path original, les folders, les fichiers
+        fichierDict = Dictionnaire comprennant name, extension, size, path, pour chaque fichier 
         ext = extenssion
         filename = nom du fichier
-        filanamrDir = Nom du fichier avec son path
+        filanameDir = Nom du fichier avec son path
     """
-    fichierDict = {}
     listeFichierDict = []
-    digit = []
-    digitListe = []
+    imageDict = {}
     dirSize = 0
     pathOriginal = os.walk("/Users/assistant/Desktop/Not_Delete")
+    a = 0
     for path, dir, files in pathOriginal:
         for file in files:
-            fileSplit = file.split(".")
-            if len(fileSplit) > 2:
-                digitSplit = fileSplit[-2]
+            fichierDict = {}
+            fileSplit = file.split(".") #fichier séparé par les points
             ext = fileSplit[-1]
             filename = fileSplit[0]
-            filenameDir = os.path.join(path, file)
-            dirSize += os.path.getsize(filenameDir)
-            fichierDict["name"] = filename
-            fichierDict["extension"] = ext
-            fichierDict["size"] = dirSize
-            fichierDict["path"] = path
-            listeFichierDict.append(fichierDict)
-    return digit
+            if file.startswith("."): #enlever le .ds_store
+                continue
+            if size: #premier argument optionnel si il y a une valeur d'entré nous allons avoir le poids dans le dict, sinon rien
+                filenameDir = os.path.join(path, file)
+                dirSize = os.path.getsize(filenameDir)
+                fichierDict["Size"] = dirSize
+            if extension: #Si un argument est ecrit, le script va seulement lister les fichier avec les extension indiqué
+                if extension == ext:
+                    fichierDict["name"] = filename
+                    fichierDict["extension"] = ext
+                    fichierDict["path"] = path
+                continue
+            if len(fileSplit) == 3: #pour trouver si c'est une image séquence on rechercher les split de trois partie
+                if filename in file:
+                    imageDict.setdefault(filename, []).append(file)
+                    fichierDict["extension"] = ext
+                    fichierDict["path"] = path
+                    for key in imageDict: # pour avoir juste le premier frame comme filename
+                        fichierDict["name"] = key
+                fichierDict["Duration"] = len(imageDict[filename])
+            else:
+                fichierDict["name"] = filename
+                fichierDict["extension"] = ext
+                fichierDict["path"] = path
+        listeFichierDict.append(fichierDict)
+    return listeFichierDict
+
+g = listingFolder()
+print g
